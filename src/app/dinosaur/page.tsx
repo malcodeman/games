fdb2497edf7d1d22bd2471ac3e1b7530196ac50f
malcodeman <26983729+malcodeman@users.noreply.github.com>
2025-01-05 23:01:20 +0100

    feat: adds dino page

---
+++ src/app/dinosaur/page.tsx
@@ -0,0 +1,7 @@
+export default function DinosaurPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[#221f22] text-[#fcfcfa]">
+      <h1>Dinosaur</h1>
+    </div>
+  );
+}
