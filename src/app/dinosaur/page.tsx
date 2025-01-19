573bf90219ae33c156d031a248783124043f497b
malcodeman <26983729+malcodeman@users.noreply.github.com>
2025-01-19 22:53:18 +0100

    feat: adds pixi

--- src/app/dinosaur/page.tsx
+++ src/app/dinosaur/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+import { Stage, Text } from "@pixi/react";
+
 export default function DinosaurPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-[#221f22] text-[#fcfcfa]">
-      <h1>Dinosaur</h1>
+      <Stage width={800} height={600} options={{ backgroundColor: "#fff" }}>
+        <Text text="Dinosaur" x={100} y={100} />
+      </Stage>
     </div>
   );
 }
