c4afa40c8d102049303c2e15c86f5bd3db83b3d2
malcodeman <26983729+malcodeman@users.noreply.github.com>
2025-01-23 11:46:35 +0100

    feat: adds ground line

--- src/app/dinosaur/page.tsx
+++ src/app/dinosaur/page.tsx
@@ -1,11 +1,20 @@
 "use client";
-import { Stage, Text } from "@pixi/react";
+import { Stage, Sprite, Graphics } from "@pixi/react";
+import { Graphics as GraphicsType } from "pixi.js";
+
+const ground = (g: GraphicsType) => {
+  g.clear();
+  g.lineStyle(1, "#ccc");
+  g.moveTo(0, 550); // Start position
+  g.lineTo(800, 550); // End position
+};
 
 export default function DinosaurPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-[#221f22] text-[#fcfcfa]">
-      <Stage width={800} height={600} options={{ backgroundColor: "#fff" }}>
-        <Text text="Dinosaur" x={100} y={100} />
+      <Stage width={800} height={600} options={{ backgroundColor: "#000" }}>
+        <Graphics draw={ground} />
+        <Sprite image="./sprites/ghost.png" x={100} y={500} />
       </Stage>
     </div>
   );
